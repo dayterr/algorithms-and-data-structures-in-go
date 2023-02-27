b64367d711694ffd882796d484185b856e72461e
Mariya Dayter <mvdayter@gmail.com>
2023-02-28 00:57:55 +0300

    added k-binary generation

--- recursion/k-binary/main.go
+++ recursion/k-binary/main.go
@@ -1,1 +1,19 @@
-package k_binary
+package main
+
+import "fmt"
+
+func generateBinary(k int, arr *[]int) {
+	if k < 1 {
+		fmt.Println(*arr)
+		return
+	}
+	(*arr)[k-1] = 0
+	generateBinary(k - 1, arr)
+	(*arr)[k-1] = 1
+	generateBinary(k - 1, arr)
+}
+
+func main() {
+	generateBinary(3, &[]int{0, 0, 0})
+	generateBinary(5, &[]int{0, 0, 0, 0, 0})
+}
